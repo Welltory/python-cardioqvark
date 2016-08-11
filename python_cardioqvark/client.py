e45054dee79b724b2ec35eb7eeb42eb6960cb7dc
Maxim Smirnov <smirnoffmg@gmail.com>
2016-08-11 16:56:59 +0300

    Added ability to get list of profiles and cardio

--- python_cardioqvark/client.py
+++ python_cardioqvark/client.py
@@ -5,17 +5,35 @@ from .base import BaseAPIClient
 
 
 class CardioQVARKClient(BaseAPIClient):
-    def get_cardiogram(self, _id):
+    def get_cardiogram(self, _id='', range_start=None, range_end=None,
+                       **params):
         url = '/cardiogram/{}'.format(_id)
-        result = self._call_api_method(url=url, server=self.api_server_url)
+        headers = {}
+        if range_start and range_end:
+            headers['Range'] = 'items={}-{}'.format(
+                range_start,
+                range_end
+            )
+        result = self._call_api_method(
+            url=url,
+            server=self.api_server_url,
+            headers=headers,
+            **params
+        )
         return result.json()
 
-    def get_patient(self, _id):
-        url = '/profile/{}'.format(_id)
-        result = self._call_api_method(url=url)
+    def get_patient(self, _id='', range_start=None, range_end=None, **params):
+        url = '/profile/{}'.format(_id or '')
+        headers = {}
+        if range_start and range_end:
+            headers['Range'] = 'items={}-{}'.format(
+                range_start,
+                range_end
+            )
+        result = self._call_api_method(url=url, **params)
         return result.json()
 
-    def get_analysis(self, _id):
+    def get_analysis(self, _id, **params):
         url = '/analysis/{}/vsr'.format(_id)
-        result = self._call_api_method(url=url)
+        result = self._call_api_method(url=url, **params)
         return result.json()
