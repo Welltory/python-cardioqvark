34ce2ed5cf84ed371253b2fa1da750c5ae1f4936
Maxim Smirnov <smirnoffmg@gmail.com>
2016-08-24 11:43:01 +0300

    travis [WIP]

--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -21,7 +21,7 @@ except:
     from urllib.request import urlopen
 
 
-GITHUB_REPO = 'budurli/python_cardioqvark'
+GITHUB_REPO = 'Welltory/python-cardioqvark'
 TRAVIS_CONFIG_FILE = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '.travis.yml')
 
